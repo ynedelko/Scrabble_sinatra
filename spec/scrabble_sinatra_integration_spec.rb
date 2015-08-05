2e3d20415c301f0e3b704c0558f7d565e763b2fe
Guest User <student@epicodus.com>
2015-08-05 11:33:16 -0700

    Create integration test spec file and write test handling correct display of scrabble score

---
+++ spec/scrabble_sinatra_integration_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe("the scrabble score path", {:type => :feature}) do
+  it("displays the correct scrabble score for a user entered word") do
+    visit('/')
+    fill_in('word', :with => 'Zeitgeist')
+    click_button('send')
+    expect(page).to have_content(19)
+  end
+end
